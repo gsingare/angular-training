00328c22712822de0dfbd23a9ecb479bf3e8269e
jyothsnasappidi <JYOTHSNA@JYOTHSNA>
2018-06-19 14:31:05 -0400

    hi

--- training/src/app/app.component.ts
+++ training/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template : '<div> I ma app <rdtech-datagrid></rdtech-datagrid> <rdtech-datagrid></rdtech-datagrid> </div>'
+  template : '<div> Welcome<datagrid></datagrid>  </div>'
 })
 
 export class AppComponent {
--- training/src/app/components/datagrid.component.ts
+++ training/src/app/components/datagrid.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'rdtech-datagrid',
+  selector: 'datagrid',
   templateUrl: "./datagrid.component.html",
   styleUrls: ["./datagrid.compnent.css"]
 })
 
 export class DatagridComponent implements OnInit {
+  public data =[{Name:"Jyothsna",Id:"1"},
+  {Name:"jyo",Id:"2"}];
 
   public ngOnInit() {
-    console.log(" iam inside the datagrid");
+    console.log(" datagrid");
 
   }
 }
