d2a71f1adb8766e7dc1e9678d6f798eb4af62351
ravalireddy13 <meravalireddy@gmail.com>
2018-06-21 00:49:48 +0530

    mychanges

--- training/src/app/app.component.ts
+++ training/src/app/app.component.ts
@@ -2,8 +2,41 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template : '<div> I ma app <rdtech-datagrid></rdtech-datagrid> <rdtech-datagrid></rdtech-datagrid> </div>'
+  templateUrl : "/app.component.html"
+
+
 })
 
 export class AppComponent {
-}
+  private users = {
+    columns : [
+      {label : "First Name", key : "firstName"},
+      {label : "Last Name",key : "lastName"},
+      {label : "Address", key: "address"},
+      {label : "Phnno",key :"phnno"}
+    ],
+    data :
+[
+  {firstName : "Ravali", lastName : "s", address : "hyd"},
+  {firstName : "Arun", lastName : "s"},
+  {firstName : "Swetha", lastName : "s", phnno : "9676165336"}
+
+] }
+private carmodels = {
+columns : [
+  {label : "Car Name", key : "carName"},
+  {label : "Model",key : "model"},
+  {label : "Year", key: "year"}
+  
+],
+data :
+[
+{carName : "Maruthi", model : "Dzire", year : "2015"},
+{carName : "Honda", model : "City", year : "2018"},
+{carName : "Hundai", model : "i20", year : "2017"}
+
+] }}
+
+
+
+ 
--- training/src/app/components/datagrid.component.ts
+++ training/src/app/components/datagrid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input} from '@angular/core';
 
 @Component({
   selector: 'rdtech-datagrid',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class DatagridComponent implements OnInit {
+ @Input() public table;
 
   public ngOnInit() {
     console.log(" iam inside the datagrid");
