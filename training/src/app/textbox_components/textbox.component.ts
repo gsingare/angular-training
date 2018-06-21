6a1a25c9308660cbc316b13f481009a1a33cac07
mohanponnapati <ponnapatimohan@gmail.com>
2018-06-21 16:03:10 -0400

    06/20/2018 class practice

--- training/src/app/textbox_components/textbox.component.ts
+++ training/src/app/textbox_components/textbox.component.ts
@@ -1,11 +1,28 @@
-import { Component, } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'mohan-textbox',
   templateUrl : "./textbox.component.html",
-  styleUrls: ['./textbox.component.scss']
+  styleUrls: ['./textbox.component.scss'] 
 })
 
 export class textComponent {
+
+  public firstname = "Please Enter your First Name";
+  public errorMessage = "";
+
+  public onTextChange($event) {
+    let value = $event.target.value;
+
+    if (value.length < 10) {
+      this.errorMessage="Requires minimum 10 characters"
+    }
+    if (value.length >= 10 && value.length <= 15) {
+      this.errorMessage="";
+    }
+    if (value.length > 15) {
+      this.errorMessage="Maximum 15 characters only"
+    }
+  }
     /**@Input() public table;*/
    }
