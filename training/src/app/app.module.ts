8bc1ecc677184b2a027d7be486466ad315695458
jyothsnasappidi <JYOTHSNA@JYOTHSNA>
2018-06-21 10:16:48 -0400

    hello

--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {DatagridComponent} from './components/datagrid.component';
 import {textboxComponent} from './Textbox/Textbox.component';
-import {buttonComponent} from './Buttons/button.component';
+import {buttonComponent} from './Buttons/button.components';
 
 @NgModule({
   declarations: [
