ce6db686c5accf8abd9bf3bdcb18cf8b34af070f
jyothsnasappidi <JYOTHSNA@JYOTHSNA>
2018-06-21 14:21:28 -0400

    hello

--- training/src/app/Buttons/button.component.ts
+++ training/src/app/Buttons/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'Buttons',
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 
 export class buttonComponent  {
- /* @Input() public table;*/
+  @Input() public table;
   
 }
