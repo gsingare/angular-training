3a976a5c7126e4c55a34d45986440ac745a1f2f3
LakshmiPoojitha <lakshmipoojitha166@gmail.com>
2018-06-22 22:21:49 -0400

    user registration

--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import {dataGridComponent} from './components/datagrid.component';
 import {textboxComponent} from './textbox/textbox.component';
 import {buttonComponent} from './buttons/buttons.component';
+import {UserRegistrationComponent} from './userRegistration/userRegistration';
 
 @NgModule({
   declarations: [
     AppComponent,
     dataGridComponent,
     textboxComponent,
-    buttonComponent
+    buttonComponent,
+    UserRegistrationComponent
 
   ],
   imports: [
---
+++ training/src/app/userRegistration/userRegistration.ts
@@ -0,0 +1,53 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'user-registration',
+  templateUrl: "./userRegistration.html",
+  styleUrls: ['./userRegistration.scss']
+})
+
+
+export class UserRegistrationComponent {  
+
+    public userDetails={email: '', password: '', username: '', number:'', emailError: '', passwordError: '', usernameError: '', numberError: '' };
+ 
+    public login(){
+         console.log('Inside the login');
+         console.log('user details: '+ JSON.stringify(this.userDetails));
+
+        
+         if(this.userDetails.username.length==0){
+            
+            this.userDetails.usernameError = 'Username cannot be empty ';
+        }else if(this.userDetails.username.length<4){
+           this.userDetails.usernameError = 'Minimum 5 characters required';
+        }
+         if(this.userDetails.email.length==0){
+             console.log('email is empty');
+             this.userDetails.emailError = 'Email cannot be empty ';
+         }else{
+            this.userDetails.emailError = null;
+         }
+
+         if(this.userDetails.password.length==0){
+             console.log('password is empty');
+             this.userDetails.passwordError = 'Password cannot be empty ';
+         }else if(this.userDetails.password.length<5){
+             this.userDetails.passwordError = 'Minimum 6 characters required';
+         }else if(this.userDetails.password.length>15){
+            this.userDetails.passwordError = 'Maximum character count is 15';
+         }else{
+            this.userDetails.passwordError = null;
+         }
+
+         if(this.userDetails.number.length==0){
+            console.log('Number cannot be empty');
+            this.userDetails.numberError = 'Number cannot be empty';
+        }else if(this.userDetails.number.length<10){
+             this.userDetails.numberError = 'Invalid Phone Number';
+        }else{
+           this.userDetails.numberError = null;
+        }
+    }
+
+}
