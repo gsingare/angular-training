f5206c690cff100915589c6b77dd93fb1ead3af4
mohanponnapati <ponnapatimohan@gmail.com>
2018-06-25 23:50:36 -0400

    06/22/2018 enable/disable login button

--- training/src/app/user_registration/user.registration.ts
+++ training/src/app/user_registration/user.registration.ts
@@ -8,37 +8,60 @@ import { Component } from '@angular/core';
 
 export class userRegistration {
   public userdetails = {firstname: "", lastname: "", email :"", password:"", 
-  firstnameerror:"", lastnameerror:"", emailerror:"", passworderror:""};
+  firstnameerror:"", lastnameerror:"", emailerror:null, passworderror:null, disableLogin: true};
 
-  public login(){
-
-    if (this.userdetails.firstname.length == 0){
-      this.userdetails.firstnameerror="First Name cannot be empty"
-     }else{
-       this.userdetails.firstnameerror=""
-     }
-
-     if (this.userdetails.lastname.length == 0){
-      this.userdetails.lastnameerror="Last Name cannot be empty"
-     }else{
-       this.userdetails.lastnameerror=""
-     }
+  public validateEmail(event){
+    console.log ("validating the email");
+    this.userdetails.email = event.target.value;
 
     if (this.userdetails.email.length == 0){
-     this.userdetails.emailerror="email cannot be empty"
-    }else{
-      this.userdetails.emailerror=""
-    }
+      this.userdetails.emailerror="email cannot be empty"
+     }else{
+       this.userdetails.emailerror=null;
+     }
+     if(this.userdetails.emailerror == null){
+       this.userdetails.disableLogin = false;
+     }
+  }
+  public validatePassword(event){
+    console.log ("validating the password");
+    this.userdetails.password = event.target.value;
 
     if (this.userdetails.password.length == 0){
       this.userdetails.passworderror="password cannot be empty"
      }else if(this.userdetails.password.length < 8){
       this.userdetails.passworderror="password requires minimum 8 characters"
      }else if(this.userdetails.password.length >= 8 && this.userdetails.password.length <= 16){
-      this.userdetails.passworderror=""
+      this.userdetails.passworderror=null;
      }else if(this.userdetails.password.length > 16){
       this.userdetails.passworderror="password should be with in 8-16 characters only"
- 
    }
+   if(this.userdetails.passworderror == null){
+    this.userdetails.disableLogin = false;
+  }else{this.userdetails.disableLogin = true;
+  }
+  }
+  public login(){
+
+    if (this.userdetails.firstname.length == 0){
+      this.userdetails.firstnameerror="First Name cannot be empty"
+     }else{
+       this.userdetails.firstnameerror=null;
+     }
+
+     if(this.userdetails.firstnameerror == null){
+      this.userdetails.disableLogin = false;
+    }
+
+     if (this.userdetails.lastname.length == 0){
+      this.userdetails.lastnameerror="Last Name cannot be empty"
+     }else{
+       this.userdetails.lastnameerror=null;
+     }
+     if(this.userdetails.lastnameerror == null){
+      this.userdetails.disableLogin = false;
+    }
+
+    
   }
 }
