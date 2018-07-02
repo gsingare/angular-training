aea0fdd18aa74baf0313d0264fb97aab896510d5
divya <divyapaladugu34@gmail.com>
2018-07-02 11:30:27 -0700

    thursday work

--- training/src/app/app.component.ts
+++ training/src/app/app.component.ts
@@ -1,32 +1,10 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  templateUrl : "./app.component.html"
+  template: '<router-outlet></router-outlet>'
 })
 
 export class AppComponent {
-  private users = {
-    buttons: [
-      {label:"submit"},
-      {label:"click"},
-    ]
-  }
-/*
-    data: [
-      {name:"divya",id:"3",dep:"cs",view:true},
-      {name:"dp",id:"9",dep:"cs",view:true}
-  ]
- 
- private companies= { 
-  columns: [
-    {label:"company name",key:"CompanyName"},
-    {label:"Id",key:"id"},
-    {label:"Dep",key:"dep"}
-  ],
-  data: [
-    {CompanyName:"capgemini", id:"3", dep:"cs"},
-    {CompanyName:"idc",id:"9",dep:"cs"}
-]
-}*/
-}
+ }
---
+++ training/src/app/users/users.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouteService} from '../service/route.service';
+@Component({
+  selector: 'user-list',
+  template : '<div>i am in List Users {{users | json}}</div>'
+})
+
+export class usersComponent {
+    public users = {};
+constructor(private routeService: RouteService){
+this.users = this.routeService.getRouteData();
+console.log("user list is" + JSON.stringify(this.users));
+}
+}
